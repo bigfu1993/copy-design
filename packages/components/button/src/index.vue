4c2896e222bba329c63637af898879b35d77bc25
bigfu <bigfu1993@163.com>
2022-12-06 18:02:57 +0800

    init

--- packages/components/button/src/index.vue
+++ packages/components/button/src/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="btn">
-    <slot name="l"></slot>
-    is button <slot name="default"></slot>
-    <slot name="r"></slot>
+  <div
+    :class="`flex-row-center ${design ? 'c-button-design' : 'c-button'}  ${primary ? 'btn-primary' : ''} ${ghost ? 'btn-ghost' : ''} ${type ? type : ''} size-${size}`"
+    :style="`font-weight: ${weight ? '700' : '400'}`">
+    <span>
+      <slot name="l"></slot>
+      <slot>按钮</slot>
+      <slot name="r"></slot>
+    </span>
+    <em :style="`width:${corners[0][0]}px;height:${corners[0][1]}px`" class="corner-border left-top-border"
+      v-if="design && corners[0]"></em>
+    <em :style="`width:${corners[1][0]}px;height:${corners[1][1]}px`" class="corner-border right-top-border"
+      v-if="design && corners[1]"></em>
+    <em :style="`width:${corners[2][0]}px;height:${corners[2][1]}px`" class="corner-border left-bottom-border"
+      v-if="design && corners[2]"></em>
+    <em :style="`width:${corners[3][0]}px;height:${corners[3][1]}px`" class="corner-border right-bottom-border"
+      v-if="design && corners[3]"></em>
   </div>
+
 </template>
 
 <script>
@@ -11,9 +24,43 @@ export default {
   name: "c-button",
 };
 </script>
-
-<style lang="less" scoped>
-.btn {
-  color: #0000ff;
-}
+<script  setup>
+import { computed } from 'vue'
+let props = defineProps({
+  design: {
+    type: Boolean,
+    default: false
+  },
+  weight: {
+    type: Boolean,
+    default: false
+  },
+  corners: {
+    type: Array,
+    default: () => []
+  },
+  primary: {
+    type: Boolean,
+    default: false
+  },
+  ghost: {
+    type: Boolean,
+    default: false
+  },
+  type: {
+    type: String,
+    default: ''
+  },
+  size: {
+    type: String,
+    default: 'normal'
+  }
+})
+let cornerStyle = computed(() => {
+  return `border`
+})
+</script>
+<style lang="less">
+@import '~#/assets/style/theme.less';
+@import './css/index.less';
 </style>
